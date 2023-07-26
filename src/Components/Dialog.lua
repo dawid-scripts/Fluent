1c43250c1276f8c4cbb2029231070fde871772c1
dawid <dobeqkontakt@gmail.com>
2023-07-26 15:50:41 +0200

    dialog!!

---
+++ src/Components/Dialog.lua
@@ -0,0 +1,156 @@
+local UserInputService = game:GetService("UserInputService")
+local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+local Camera = workspace.CurrentCamera
+
+local Root = script.Parent.Parent
+local Flipper = require(Root.Packages.Flipper)
+local Creator = require(Root.Creator)
+
+local Spring = Flipper.Spring.new
+local Instant = Flipper.Instant.new
+local New = Creator.New
+
+local Dialog = {
+    Window = nil
+}
+
+function Dialog:Create()
+    local NewDialog = {
+        Buttons = 0
+    }
+
+    NewDialog.TintFrame = New("TextButton", {
+        Text = "",
+        Size = UDim2.fromScale(1, 1),
+        BackgroundColor3 = Color3.fromRGB(0, 0, 0),
+        BackgroundTransparency = 1,
+        Parent = Dialog.Window.Root
+    }, {
+        New("UICorner", {
+            CornerRadius = UDim.new(0, 8)
+        })
+    })
+
+    local TintMotor, TintTransparency = Creator.SpringMotor(1, NewDialog.TintFrame, "BackgroundTransparency")
+
+    NewDialog.ButtonHolder = New("Frame", {
+        Size = UDim2.new(1, -40, 1, -40),
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        Position = UDim2.fromScale(0.5, 0.5),
+        BackgroundTransparency = 1,
+    }, {
+        New("UIListLayout", {
+            Padding = UDim.new(0, 10),
+            FillDirection = Enum.FillDirection.Horizontal,
+            HorizontalAlignment = Enum.HorizontalAlignment.Center,
+            SortOrder = Enum.SortOrder.LayoutOrder
+        })
+    })
+
+    NewDialog.ButtonHolderFrame = New("Frame", {
+        Size = UDim2.new(1, 0, 0, 70),
+        Position = UDim2.new(0, 0, 1, -70),
+        ThemeTag = {
+            BackgroundColor3 = "DialogHolder"
+        }
+    }, {
+        New("Frame", {
+            Size = UDim2.new(1, 0, 0, 1),
+            ThemeTag = {
+                BackgroundColor3 = "DialogLine"
+            }
+        }),
+        NewDialog.ButtonHolder
+    })
+
+    NewDialog.Title = New("TextLabel", {
+        FontFace = Font.new(
+            "rbxasset://fonts/families/GothamSSm.json",
+            Enum.FontWeight.SemiBold,
+            Enum.FontStyle.Normal
+        ),
+        Text = "Dialog",
+        TextColor3 = Color3.fromRGB(240, 240, 240),
+        TextSize = 22,
+        TextXAlignment = Enum.TextXAlignment.Left,
+        Size = UDim2.new(1, 0, 0, 22),
+        Position = UDim2.fromOffset(20, 25),
+        BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+        BackgroundTransparency = 1,
+        ThemeTag = {
+            TextColor3 = "ElementTitle"
+        }
+    }) 
+
+    NewDialog.Scale = New("UIScale", {
+        Scale = 1
+    })
+
+    local ScaleMotor, Scale = Creator.SpringMotor(1.1, NewDialog.Scale, "Scale")
+
+    NewDialog.Root = New("CanvasGroup", {
+        Size = UDim2.fromOffset(300, 165),
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        Position = UDim2.fromScale(0.5, 0.5),
+        GroupTransparency = 1,
+        Parent = NewDialog.TintFrame,
+        ThemeTag = {
+            BackgroundColor3 = "DialogBackground"
+        }
+    }, {
+        New("UICorner", {
+            CornerRadius = UDim.new(0, 8)
+        }),
+        New("UIStroke", {
+            Transparency = 0.5,
+            ThemeTag = {
+                Color = "DialogStroke"
+            }
+        }),
+        NewDialog.Scale,
+        NewDialog.Title,
+        NewDialog.ButtonHolderFrame,
+    })
+
+    local RootMotor, RootTransparency = Creator.SpringMotor(1, NewDialog.Root, "GroupTransparency")
+
+    function NewDialog:Open()
+        NewDialog.Scale.Scale = 1.1
+        TintTransparency(0.75)
+        RootTransparency(0)
+        Scale(1)
+    end
+
+    function NewDialog:Close()
+        TintTransparency(1)
+        RootTransparency(1)
+        Scale(1.1)
+        NewDialog.Root.UIStroke:Destroy()
+        task.wait(0.15)
+        NewDialog.TintFrame:Destroy()   
+    end
+
+    function NewDialog:Button(Title, Callback)
+        NewDialog.Buttons = NewDialog.Buttons + 1
+
+        local Button = require(Root.Components.Button)("", NewDialog.ButtonHolder)
+        Button.Title.Text = Title
+
+        for _, Btn in next, NewDialog.ButtonHolder:GetChildren() do
+            if Btn:IsA("TextButton") then
+                Btn.Size = UDim2.new(1 / NewDialog.Buttons, -(( (NewDialog.Buttons - 1) * 10) / NewDialog.Buttons), 0, 32)
+            end
+        end
+
+        Creator.AddSignal(Button.Frame.MouseButton1Click, function()
+            require(Root):SafeCallback(Callback)
+            NewDialog:Close()
+        end)
+
+        return Button
+    end
+
+    return NewDialog
+end
+
+return Dialog
